74da239b5c632c5f4aa1f9d6f49fd9da03177ca0
Steven Budi <steven9c37@gmail.com>
2022-05-24 10:43:58 +0700

    install md5 + datatables

---
+++ app/Views/index.php
@@ -0,0 +1,7 @@
+<?= $this->extend('template/layout') ?>
+
+<?= $this->section('content') ?>
+
+<h1>Dashboard</h1>
+
+<?= $this->endSection() ?>
---
+++ app/Views/template/layout.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta http-equiv="x-ua-compatible" content="ie=edge" />
+    <title>Labkom</title>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" />
+    <link rel="stylesheet" href="<?=base_url('md5/css/mdb.min.css')?>" />
+    <link rel="stylesheet" href="<?=base_url('md5/css/admin.css')?>" />
+    <link href="<?= base_url('datatables/datatables.min.css'); ?>" rel="stylesheet">
+    <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.min.js" integrity="sha512-d9xgZrVZpmmQlfonhQUvTR7lMPtO7NkZMkA0ABN3PHCbKA5nqylQ/yWlFAyY6hYgdF1Qh6nYiuADWwKB4C2WSw==" crossorigin="anonymous"></script> -->
+</head>
+
+<body>
+    <header>
+        <?= $this->include('template/sidebar.php')?>
+        <?= $this->include('template/navbar.php')?>
+    </header>
+
+    <main style="margin-top: 58px">
+        <div class="container pt-4">
+            <?= $this->renderSection('content')?>
+        </div>
+    </main>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" crossorigin="anonymous"></script>
+    <script src="<?= base_url('md5/js/mdb.min.js'); ?>" crossorigin="anonymous"></script>
+    <script src="<?= base_url('md5/js/admin.js'); ?>" crossorigin="anonymous"></script>
+    <script src="<?= base_url('datatables/datatables.min.js'); ?>"></script>
+</body>
+
+</html>
