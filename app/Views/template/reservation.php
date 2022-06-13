10189956f93d084bec0b5f4a7d98ba2b3278b666
Steven Budi <steven9c37@gmail.com>
2022-06-13 15:04:18 +0700

    fixing form

--- app/Views/template/reservation.php
+++ app/Views/template/reservation.php
@@ -9,14 +9,14 @@
                 </button>
             </div>
             <div class="modal-body">
-                <form action="<?= base_url('/labkom/pesan') ?>" method="POST" enctype="multipart/form-data" id="logout-form">
-                    <? csrf_field() ?>
+                <form action="<?= base_url('/labkom/pesan') ?>" method="POST" enctype="multipart/form-data" id="reservation-form">
+                    <?= csrf_field() ?>
                     <div class="mb-3">
                         <label for="labkom-opt">Laboratorium Komputer</label>
                         <select name="labkom-opt" id="labkom-opt">
                             <option value="rpl">Laboratorium Software Engineering</option>
-                            <option value="tkj">Laboratorium Multimedia Studio</option>
-                            <option value="mulmed">Laboratorium Computer Network and Instrumentation</option>
+                            <option value="mulmed">Laboratorium Multimedia Studio</option>
+                            <option value="tkj">Laboratorium Computer Network and Instrumentation</option>
                         </select>
                     </div>
 
@@ -35,7 +35,7 @@
                         <input type="number" name="duration" id="reser-duration" max="4" min="1">
                         <p>Untuk member non-civitas UNS wajib membayar biaya peminjaman sebesar Rp. 25.000/Jam</p>
                     </div>
-                    
+
                     <div class="mb-3">
                         <label for="reser-notes">Catatan Keterangan</label>
                         <textarea name="reser-notes" id="reser-notes" rows="10"></textarea>
@@ -50,21 +50,20 @@
 </div>
 
 <script>
-    const limitDate = () => {
-        const dateA = new Date();
-        const dateB = new Date();
-        dateB.setDate(dateA.getDate() + 14);
-        dateA.setDate(dateA.getDate() + 1);
-        const minDate = `${dateA.getFullYear()}-${dateA.getMonth() < 10 ? "0" + (dateA.getMonth() + 1).toString() : (dateA.getMonth() + 1)}-${dateA.getDate() < 10 ? "0" + dateA.getDate().toString() : dateA.getDate()}`;
-        const maxDate = `${dateB.getFullYear()}-${dateB.getMonth() < 10 ? "0" + (dateB.getMonth() + 1).toString() : (dateB.getMonth() + 1)}-${dateB.getDate() < 10 ? "0" + dateB.getDate().toString() : dateB.getDate()}`;
-        try {
-            document.getElementById("reser-date").setAttribute("min", minDate);
-            document.getElementById("reser-date").setAttribute("max", maxDate);
-        } catch (error) {
-            console.error();
-        }
-    }
-    
+    $('#reservation-form').submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            url: $(this).attr('action'),
+            method: $(this).attr('method'),
+            data: new FormData(this),
+            processData: false,
+            contentType: false,
+            success : function(response){
+                
+            }
+        })
+    });
+
     $(document).ready(() => {
         limitDate();
     })
